99a2b3c7c453f3f63a561c80a7d01a786ff91a27
Mike Hearn <mike@plan99.net>
2014-12-09 15:54:53 +0100

    Use javapackager not javafxpackager

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ cp examples/target/updatefx-examples-1.1.jar examples/website/builds/2.jar
 mv examples/src/main/java/ExampleApp.java.bak examples/src/main/java/ExampleApp.java
 java -jar app/target/updatefx-app-1.1.jar --url=http://localhost:8000/ examples/website
 rm -rf examples/deploy/*
-$JAVA_HOME/bin/javafxpackager -deploy -outdir examples/deploy/ -outfile UFXExample.dmg -name UFXExample -native dmg -appclass ExampleApp -srcfiles examples/website/builds/1.jar
+$JAVA_HOME/bin/javapackager -deploy -outdir examples/deploy/ -outfile UFXExample.dmg -name UFXExample -native dmg -appclass ExampleApp -srcfiles examples/website/builds/1.jar
